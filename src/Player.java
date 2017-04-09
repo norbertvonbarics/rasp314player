0dd0fc6bfcd6e24f85c20e72f8635c17ea9827e3
norbert barics <norbert.barics@gmail.com>
2017-04-09 19:57:38 +0200

    basicplayer play method working

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,24 @@
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javazoom.jlgui.basicplayer.BasicController;
 import javazoom.jlgui.basicplayer.BasicPlayer;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 
 public class Player {
-  public void playmp3 (){
+  public void playmp3() {
     String songName = "Win sound.mp3";
-    String pathToMp3 = System.getProperty("./assets") +"/"+ songName;
-    BasicPlayer player = new BasicPlayer();
+    String pathToMp3 = "./assets" +"/"+ songName;
+    BasicPlayer myPlayer = new BasicPlayer();
+    BasicController playerController = (BasicController) myPlayer;
+    File file = new File(pathToMp3);
     try {
-      player.open(new URL("file:///" + pathToMp3));
-      player.play();
-    } catch (BasicPlayerException | MalformedURLException e) {
-      e.printStackTrace();
+      playerController.open(file);
+      playerController.play();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
   }
 }
