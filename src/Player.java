1d22618fc18481093742ce442e7ce8bbf75a39ae
norbert barics <norbert.barics@gmail.com>
2017-04-09 22:00:36 +0200

    add play scanner function

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 import javazoom.jlgui.basicplayer.BasicController;
 import javazoom.jlgui.basicplayer.BasicPlayer;
@@ -8,17 +9,24 @@ import javazoom.jlgui.basicplayer.BasicPlayerException;
 
 public class Player {
   public void playmp3() {
-    String songName = "Win sound.mp3";
-    String pathToMp3 = "./assets" +"/"+ songName;
-    BasicPlayer myPlayer = new BasicPlayer();
-    BasicController playerController = (BasicController) myPlayer;
-    File file = new File(pathToMp3);
-    try {
-      playerController.open(file);
-      playerController.play();
+    System.out.println("Please press play!");
+    Scanner myScanner = new Scanner(System.in);
+    String userControl = myScanner.nextLine();
 
-    } catch (Exception ex) {
-      ex.printStackTrace();
+    if (userControl.toLowerCase().equals("play")) {
+      String songName = "Win sound.mp3";
+      String pathToMp3 = "./assets" + "/" + songName;
+      BasicPlayer myPlayer = new BasicPlayer();
+      BasicController playerController = (BasicController) myPlayer;
+      File file = new File(pathToMp3);
+      try {
+        playerController.open(file);
+        playerController.play();
+
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
     }
+    System.out.println("The song is over!");
   }
 }
